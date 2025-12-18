1565f2a5042993b26358a61c48094b4642764620
Sayema Qureshi <qureshisayema47@gmail.com>
2025-12-18 07:13:13 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,21 @@
+# Strings and arrays
+arr1 = [1, 2, 3, 4]
+arr1.reverse()
+print(arr1)
+arr1.append(0)
+print(arr1)
+arr1.pop(3)
+print(arr1)
+arr1.remove(3)
+print(arr1)
+arr1.insert(1, 1)
+print(arr1)
+arr1.sort()
+print(arr1)
+
+
+str1 = "sayema"
+str1.split()
+print(str1)
+str1.isdigit()
+
